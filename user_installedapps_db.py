f0c06cf4f6973c335c46f9869885cc483e0a42d4
he <ffpphh@outlook.com>
2017-04-29 22:22:12 +0800

    test db

--- user_installedapps_db.py
+++ user_installedapps_db.py
@@ -1,10 +1,10 @@
 import pymongo
 
-client = pymongo.MongoClient('120.24.223.44', 27017)
+client = pymongo.MongoClient('localhost', 27017)
 
-db = client['pre']
+db = client.pre
 
 collection = db['user_installedapps']
 
-for user in collection.find({"userId":'1'}):
+for user in collection.find({"userId":1}):
     print(user)
