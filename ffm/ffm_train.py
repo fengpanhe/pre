05e7da276aa88eebbc96edbb58605804cc204da2
he <ffpphh@outlook.com>
2017-05-13 02:07:18 +0800

    添加一些必要的的打印信息ffm

--- ffm/ffm_train.py
+++ ffm/ffm_train.py
@@ -11,7 +11,7 @@ class FfmTrain(object):
         self.ffm_program_path = ffm_program_path
 
     def init_data(self):
-
+        print('init_data')
         dirs = os.listdir(self.data_path)
         if 'train_ffm_data' in dirs and 'test_ffm_data' in dirs:
             return True
@@ -45,7 +45,7 @@ class FfmTrain(object):
         subprocess.call(shell_command, shell=True, cwd=test_ffm_data_path)
 
     def train_validation(self):
-
+        print('train_validation')
         train_tr_ffm = self.data_path + 'train_ffm_data/train_tr_ffm'
         train_va_ffm = self.data_path + 'train_ffm_data/train_va_ffm'
         shell_command = './ffm-train '
@@ -55,6 +55,7 @@ class FfmTrain(object):
         shell_command += ' --auto-stop '
         shell_command += train_tr_ffm
         shell_command += ' ' + self.data_path + 'model'
+        print(shell_command)
         s = subprocess.check_output(
             shell_command, shell=True, cwd=self.ffm_program_path)
         file = open(self.data_path + 'tr_va_logloss', 'w')
@@ -62,12 +63,14 @@ class FfmTrain(object):
         file.close()
 
     def predict(self):
-
+        print('predict')
         test_ffm = self.data_path + 'test_ffm_data/test_ffm'
         shell_command = './ffm-predict '
         shell_command += test_ffm + ' '
         shell_command += self.data_path + 'model' + ' '
         shell_command += self.data_path + 'output'
+        print(shell_command)
         subprocess.call(shell_command, shell=True, cwd=self.ffm_program_path)
         shell_command = 'nl -s \',\' ' + 'output > submission.csv'
+        print(shell_command)
         subprocess.call(shell_command, shell=True, cwd=self.data_path)
